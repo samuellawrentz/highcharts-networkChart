107fc428f186694c03578781f1f622d205283090
Samuel Lawrentz <samuellawrentz@gmail.com>
2018-07-19 00:14:51 +0530

    Separated the label renderer and bubble renderer

--- script.js
+++ script.js
@@ -1,108 +1,116 @@
-function drawChart(options){
+function drawChart(options) {
     var chart = this;
     var renderer = chart.renderer;
-    var centerX = chart.chartWidth/2 + chart.plotLeft;
-    var centerY = chart.chartHeight/2;
-    var angleDifference = 360/options.length;
+    var centerX = chart.chartWidth / 2 + chart.plotLeft;
+    var centerY = chart.chartHeight / 2;
+    var angleDifference = (360 / options.length) * (Math.PI/180);
     var angle = 0;
     var bubbleStyle = {
         fill: '#9cc1e1',
         stroke: 'white',
-        'stroke-width': 3,            
-        zIndex:3
+        'stroke-width': 3,
+        zIndex: 3
     };
 
     var lineStyle = {
         stroke: '#d1e4f1',
-        'stroke-width': 20,        
-        zIndex:0
+        'stroke-width': 20,
+        zIndex: 0
     }
-    
 
-    var getRandomArbitrary = function(min, max) {
+    var points = [];
+
+
+    var getRandomArbitrary = function (min, max) {
         return Math.random() * (max - min) + min;
-      }
+    }
 
-    var renderPoints = function(){
+    var renderPoints = function () {
         var circle = renderer.circle(centerX, centerY, 50).attr({
             fill: '#5ba2dc',
             stroke: 'white',
-            'stroke-width': 2,            
-            zIndex:3
+            'stroke-width': 2,
+            zIndex: 3
         })
-        .add();   
+            .add();
 
-        options.forEach((element) => {
+        options.forEach(element => {
             var length = getRandomArbitrary(100, 250);
             var radius = getRandomArbitrary(20, 40);
             renderBubbles(length, radius, element);
         });
     }
 
-    var renderLabels = function(){
-        
+    var renderLabels = function () {
+        points.forEach(point => {
+            var circle = point.circle;
+            var group = point.group;
+            var text = point.name;
+            var circleBBox = circle.getBBox();
+            var labelX = circleBBox.x + circleBBox.width + 10;
+            var labelY = circleBBox.y + circleBBox.height / 2;
+
+            var label = renderer.text(text, labelX, labelY)
+            .attr({
+                zIndex: 10
+            })
+            .add(group);
+        });
     }
 
-    
 
-    var renderBubbles = function(length, radius, element){    
-        var group =  renderer.g('group').add();
-        var circle = renderer.circle(centerX, centerY + length, radius).attr(bubbleStyle)
-        .add(group);
 
-        var circleBBox = circle.getBBox();
-        var labelX = circleBBox.x + circleBBox.width + 10;
-        var labelY = circleBBox.y + circleBBox.height/2; 
-    
-    
-        var path = renderer.path(['M', centerX, centerY, 'l', 0, length])
-        .attr(lineStyle)
-        .add(group);
+    var renderBubbles = function (length, radius, element) {
+        var group = renderer.g('group').add();
+        var x = centerX + length * Math.cos(angle);
+        var y = centerY + length * Math.sin(angle);
+        var circle = renderer.circle(x, y, radius).attr(bubbleStyle)
+            .add(group);
 
-        var label = renderer.text(element, labelX, labelY).add(group)
-        .attr({
-            rotation:-angle,
-            zIndex:10
+        var path = renderer.path(['M', centerX, centerY, 'L', x, y])
+            .attr(lineStyle)
+            .add(group);
+
+        angle += angleDifference;
+
+        points.push({
+            circle: circle,
+            path: path,
+            group: group,
+            name: element
         });
-    
-        group.attr({
-            x:centerX,
-            y:centerY,
-            rotation:angle
-        });
-
-        angle+=angleDifference;
     }
 
     renderPoints();
+    renderLabels();
 
 }
 
 
 
 
-function NetworkChart(settings){
+function NetworkChart(settings) {
     this.data = settings.data;
 }
 
-NetworkChart.prototype.draw = function(){
+NetworkChart.prototype.draw = function () {
     var self = this;
     Highcharts.chart('container', {
         chart: {
-          backgroundColor: 'white',
-          events: {
-            load: function(){
-                drawChart.call(this, self.data);
+            backgroundColor: 'white',
+            events: {
+                load: function () {
+                    drawChart.call(this, self.data);
+                }
             }
-          }
         },
-        title: false      
-      });
+        title: false
+    });
 }
 
 var netChart = new NetworkChart({
-    data:['Software Developer', 'Java Developer', 'Data Scientist', , 'Team lead', 'Senior Developer',
-    'Software Engineer','Database Engineer', 'System Admin', 'Team lead', 'Senior Developer']
+    data: ['Software Developer', 'Java Developer', 'Data Scientist', , 'Team lead', 'Senior Developer',
+        'Software Engineer', 'Database Engineer', 'System Admin', 'Team lead', 'Senior Developer']
 });
 
 netChart.draw();
